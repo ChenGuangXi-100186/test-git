85ffba0dc09f6192aa76daa283eaefb030fc21c4
cgx <cgx15346369694@163.com>
2021-11-02 18:57:51 +0800

    test-github-push

--- src/main/java/com/cgx/test_git/GitTest.java
+++ src/main/java/com/cgx/test_git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("test-git4");
         System.out.println("冲突测试-master");
         System.out.println("冲突测试-hos_fix");
+        System.out.println("test-github-push");
     }
 }
